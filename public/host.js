a2a269c9bc4dc02075144353c217c64c34a46ff8
Sameer Ali <sali77419@gmail.com>
2021-11-06 15:19:09 +0500

    Add files via upload

---
+++ public/host.js
@@ -0,0 +1,23 @@
+const socket = io()
+const active = document.querySelector('.js-active')
+const buzzList = document.querySelector('.js-buzzes')
+const clear = document.querySelector('.js-clear')
+
+socket.on('active', (numberActive) => {
+  active.innerText = `Number of participants: ${numberActive}`
+})
+
+socket.on('buzzes', (buzzes) => {
+  buzzList.innerHTML = buzzes
+    .map(buzz => {
+      const p = buzz.split('-')
+      return { name: p[0], team: p[1] }
+    })
+    .map(user => `<li>${user.name} on ${user.team}</li>`)
+    .join('')
+})
+
+clear.addEventListener('click', () => {
+  socket.emit('clear')
+})
+
